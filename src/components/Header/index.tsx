582192e5a35dbb89ea07c2acc2efe568d11745d5
natcurti <naticurti01@gmail.com>
2024-03-17 16:55:30 -0300

    Create component header and routes with react router dom

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import logo from "src/assets/images/Logo.svg";
+
+const Header = () => {
+  return (
+    <header className="bg-banner bg-cover bg-center h-40 flex items-center justify-center">
+      <h1>
+        <img src={logo} alt="Logo World Ranks" />
+      </h1>
+    </header>
+  );
+};
+
+export default Header;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,21 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        beVietnam: ["Be Vietnam Pro", "sans-serif"],
+      },
+      backgroundImage: {
+        banner: "url('/src/assets/images/hero-image-wr.jpg')",
+      },
+      colors: {
+        "dark-gray": "#1B1D1F",
+        "medium-gray": "#282B30",
+        "light-gray": "#6C727F",
+        offwhite: "#D2D5DA",
+        blue: "#4E80EE",
+      },
+    },
   },
   plugins: [],
 };
