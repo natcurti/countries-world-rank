e450a8fb6e563507a92bd15f02a9ec4eaf48e00a
natcurti <naticurti01@gmail.com>
2024-03-22 11:33:30 -0300

    Adjust layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import Header from "./components/Header";
 import { CountriesContextProvider } from "./context/CountriesContext";
 import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
@@ -6,6 +7,7 @@ function App() {
   return (
     <BrowserRouter>
       <CountriesContextProvider>
+        <Header />
         <Routes>
           <Route path="/" element={<Home />} />
         </Routes>
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,5 @@
 import { useContext } from "react";
 import CountriesContainer from "src/components/CountriesContainer";
-import Header from "src/components/Header";
 import Input from "src/components/Input";
 import Select from "src/components/Select";
 import Status from "src/components/Status";
@@ -10,21 +9,18 @@ import { CountriesContext } from "src/context/CountriesContext";
 const Home = () => {
   const { countries } = useContext(CountriesContext);
   return (
-    <>
-      <Header />
-      <main className="flex flex-col gap-5 p-5 w-full shadow-lg bg-blue-50 lg:w-4/5 lg:absolute lg:top-40 lg:rounded-2xl lg:left-2/4 lg:-translate-x-1/2 dark:bg-dark-gray">
-        <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
-          <h2 className="text-base sm:text-xl text-blue-900 font-bold dark:text-light-gray">
-            Found {countries.length === 0 ? "..." : countries.length} countries
-          </h2>
-          <Input />
-        </div>
-        <Select />
-        <Tags />
-        <Status />
-        <CountriesContainer />
-      </main>
-    </>
+    <main className="flex flex-col gap-5 p-5 w-full shadow-lg bg-blue-50 lg:w-4/5 lg:rounded-2xl lg:mx-auto lg:-translate-y-10 dark:bg-dark-gray">
+      <div className="flex flex-col gap-3 sm:flex-row sm:items-center sm:justify-between">
+        <h2 className="text-base sm:text-xl text-blue-900 font-bold dark:text-light-gray">
+          Found {countries.length === 0 ? "..." : countries.length} countries
+        </h2>
+        <Input />
+      </div>
+      <Select />
+      <Tags />
+      <Status />
+      <CountriesContainer />
+    </main>
   );
 };
 
